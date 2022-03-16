819308c0ec4cf4332d6c2ad636b4fd2647246f70
Thayara Siqueira <thay.siqu@gmail.com>
2022-03-16 17:22:05 -0600

    corrige requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,10 +2,11 @@ const data = require('../data/zoo_data');
 
 const { employees } = data;
 
-console.log(employees);
-
 function getEmployeeByName(employeeName) {
-  return employees.filter(({ firstName, lastName }) =>
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find(({ firstName, lastName }) =>
     firstName === employeeName || lastName === employeeName);
 }
 
