816d377be286612cd1885d3e9fbcb5c9e60050d5
Thayara Siqueira <thay.siqu@gmail.com>
2022-03-16 13:51:52 -0600

    corrige lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,10 +1,11 @@
 const data = require('../data/zoo_data');
+
 const { species } = data;
 
 function getAnimalsOlderThan(animal, minAge) {
-  const animals = species.filter(({name}) => name === animal);
-  const [ { residents } ] = species;
-  const verify = residents.every(({age}) => age >= minAge);
+  const animals = species.filter(({ name }) => name === animal);
+  const [{ residents }] = animals;
+  const verify = residents.every(({ age }) => age >= minAge);
   return verify;
 }
 
