12152f61f1049969412e21ff2e015895de3e08e1
Thayara Siqueira <thay.siqu@gmail.com>
2022-03-16 16:54:18 -0600

    add requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+const { adult, senior, child } = prices;
+const teste = {};
+
 function countEntrants(entrants) {
+  const children = entrants.filter(({age}) => age < 18);
+  const adults = entrants.filter(({age}) => age >= 18 && age < 50);
+  const seniors = entrants.filter(({age}) => age >= 50);
+  return { child:children.length, adult:adults.length, senior:seniors.length};
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  const array = Object.values(entrants);
+  if(array.length === 0) {
+    return 0;
+  }
+  const list = countEntrants(entrants);
+  const getPeople = Object.values(list);
+  const result = (getPeople[0] * child) + (getPeople[1] * adult) + (getPeople[2] * senior);
+  return result;
+  
 }
+
+console.log(calculateEntry());
 
 module.exports = { calculateEntry, countEntrants };
