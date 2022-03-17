780e0062bad442056d19775961709281377f0447
Thayara Siqueira <thay.siqu@gmail.com>
2022-03-17 11:09:14 -0600

    add requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -6,9 +6,8 @@ function getSpeciesByIds(...ids) {
   const array = ids;
   if (!ids) {
     return [];
-  }
-  array.forEach((element) => species.filter(({ id }) => id === element));
+  } 
+  return species.filter(({ id }) => id === array[0] || id === array[1]);
 }
-// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
-// return species.filter(({id}) => id === ids ); //funciona para um id apenas
+
 module.exports = getSpeciesByIds;
