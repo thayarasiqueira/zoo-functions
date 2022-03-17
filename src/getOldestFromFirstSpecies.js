9d117ebc1581d21e0b5ade2b46afc87e83bce73e
Thayara Siqueira <thay.siqu@gmail.com>
2022-03-17 16:32:42 -0600

    correcao lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,17 +4,15 @@ const { employees } = data;
 const { species } = data;
 
 function getOldestFromFirstSpecies(idEmployee) {
-  const employee = employees.find(({ id }) =>  id === idEmployee);
-  const responsibleFor = employee.responsibleFor
+  const employee = employees.find(({ id }) => id === idEmployee);
+  const { responsibleFor } = employee;
   const animals = species.find(({ id }) => id === responsibleFor[0]);
-  const resident = animals.residents
-  const animalAge = resident.reduce((prev,curr) => curr.age > prev ? curr.age : prev, 0);
+  const resident = animals.residents;
+  const animalAge = resident.reduce((prev, curr) => (curr.age > prev ? curr.age : prev, 0));
   const animal = resident.filter(({ age }) => age === animalAge);
   const [{ name, sex, age }] = animal;
-  const result = [ name, sex, age ];
+  const result = [name, sex, age];
   return result;
-}
-
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'))
+};
 
 module.exports = getOldestFromFirstSpecies;
