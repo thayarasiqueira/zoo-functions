3a77adcfc10517106e5e669e82964f5ed715ea54
Thayara Siqueira <thay.siqu@gmail.com>
2022-03-19 14:07:55 -0600

    add requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees } = data;
+const { species } = data;
+
+// function isValid(id) {
+//   const employee = Object.values(id).toString();
+//   const verify = employees.some((e) => e.id === employee || e.firstName === employee || e.lastName === employee);
+//   if (verify === false) {
+//     throw new Error('Informações inválidas');
+//   }
+// }
+
+function allEmployees() {
+  return employees.map(({ id, firstName, lastName, responsibleFor }) => ({
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: species.filter((animal) => responsibleFor.find((ids) => animal.id === ids)).map(({ name }) => name),
+    locations: species.filter((animal) => responsibleFor.find((ids) => animal.id === ids)).map(({ location }) => location)
+  }))
 }
 
+function getEmployeesCoverage(employee) {
+  if (!employee) {
+    return allEmployees();
+  }
+  const parameter = Object.values(employee).toString();
+  const obj = employees.find(({ firstName, lastName, id }) => parameter === firstName || parameter === lastName || parameter === id)
+  if (obj === undefined) throw new Error('Informações inválidas')
+  const { id, firstName, lastName, responsibleFor } = obj;
+  const result = {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: species.filter((animal) => responsibleFor.find((ids) => animal.id === ids)).map(({ name }) => name),
+    locations: species.filter((animal) => responsibleFor.find((ids) => animal.id === ids)).map(({ location }) => location)
+  };
+  return result;
+}
+
+console.log(getEmployeesCoverage({ name: 'Shaa'}));
 module.exports = getEmployeesCoverage;
